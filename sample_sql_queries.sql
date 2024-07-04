3b81b53daebbe0fc4f5d4521a1f583b8ee09e0e0
unknown <you@example.com>
2024-07-04 16:07:06 +0530

    sample SQL file

---
+++ sample_sql_queries.sql
@@ -0,0 +1,10 @@
+1)Retrieve data with specific record,using WHERE condition.
+ SQL> SELECT * FROM STUDENT WHERE COURSE_ID='1021';  
+
+2)Adding a new record.
+SQL>INSERT INTO COURSE VALUES(1033,’HARI’,2000,103,400);
+
+3)Updated an old record.
+SQL> UPDATE STUDENT SET GRADE = ‘C’ WHERE STUDENT_ID = 1;  
+
+ 
